cb75225d8e284d92852bd2a7b3fb8643fb476aba
DiegoV22 <122640502+DiegoV22@users.noreply.github.com>
2025-11-25 02:28:25 +0000

    1

---
+++ src/test/java/LibroTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LibroTest {
+
+    @Test
+    void testPrestarLibro() {
+        Libro libro = new Libro("1984", "Orwell");
+        libro.prestar();
+        assertFalse(libro.estaDisponible());
+    }
+
+        @Test
+    void testDevolverLibro() {
+        Libro libro = new Libro("1984", "Orwell");
+        libro.devolver();
+        asserTrue(libro.estaDisponible());
+    }
+
+    @Test
+    void testPrestarLibroNoDisponible() {
+        Libro libro = new Libro("1984", "Orwell");
+        libro.prestar();
+        assertThrows(IllegalStateException.class, libro::prestar);
+    }
+}
